9ebcfdb19419cd80077f9029107ccabeaf2c1f8e
vstrk1 <vladisen1244@gmail.com>
2016-10-14 13:35:28 +0300

    Added additional class with all implementation of homework

---
+++ Array/LevelTask.h
@@ -0,0 +1,19 @@
+//
+//  LevelTask.h
+//  Array
+//
+//  Created by Stuparenko Vladyslav on 10/14/16.
+//  Copyright © 2016 Stuparenko Vladyslav. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LevelTask : NSObject
+
+
++ (void) runMaserTask;
+
++ (void) runLearnerTask;
+
+
+@end
