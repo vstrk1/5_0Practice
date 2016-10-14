8e551caec578c50030e2c992459bb4a3aa2a4e19
vstrk1 <vladisen1244@gmail.com>
2016-10-14 13:37:08 +0300

    123

--- Array/AppDelegate.m
+++ Array/AppDelegate.m
@@ -7,14 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "Human.h"
-#import "Runner.h"
-#import "Cycler.h"
-#import "Swimer.h"
-#import "Biorobot.h"
-#import "Animal.h"
-#import "Leopard.h"
-#import "Ox.h"
+#import "LevelTask.h"
 
 @interface AppDelegate ()
 
@@ -22,72 +15,11 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-//    Human* defaultHuman = [[Human alloc] initWithParameters:@"Joseph" andSex:@"M" andAge:21 andHeigh:178.f andWeigh:77.f];
-//    Runner* runner = [[Runner alloc] initWithParameters:@"Julia" andSex:@"F" andAge:28 andHeigh:166.6f andWeigh:54.3f];
-//    Cycler* cycler = [[Cycler alloc] initWithParameters:@"Yaroslav" andSex:@"M" andAge:29 andHeigh:179.22f andWeigh:70.f];
-//    Swimer* swimer = [[Swimer alloc] initWithParameters:@"Ustas" andSex:@"M" andAge:45 andHeigh:198.4f andWeigh:122.5f];
-//    Biorobot* robot = [[Biorobot alloc] initWithParameters:@"Angela" andSex:@"F" andAge:40 andHeigh:164.4f andWeigh:44.f andTypeOfProthesis:@"Hand"];
+    [LevelTask runMaserTask];
     
-    
-//    NSArray* stackOfPeople = [NSArray arrayWithObjects:defaultHuman, runner, cycler, swimer, robot, nil];
-    
-//    for (Human* human in stackOfPeople) {
-    
-//    for (Human* human in [stackOfPeople reverseObjectEnumerator]) {
-//        NSLog(@"__________________________");
-//        NSLog(@"Name: %@", human.name);
-//        NSLog(@"Sex: %@", human.sex);
-//        NSLog(@"Age: %ld", human.age);
-//        NSLog(@"Heigh: %.01f", human.heigh);
-//        NSLog(@"Weigh: %.01f", human.weigh);
-//        [human move];
-//        
-//        if ([human isKindOfClass:[Biorobot class]]) {
-//            Biorobot* tempRobot = (Biorobot* )human;
-//            NSLog(@"Current battery: %ld%%", tempRobot.powerBalance);
-//        }
-//    }
-    
-    Human* defaultHuman = [[Human alloc] initWithParameters:@"Joseph" andSex:@"M" andAge:21 andHeigh:178.f andWeigh:77.f];
-    Runner* runner = [[Runner alloc] initWithParameters:@"Julia" andSex:@"F" andAge:28 andHeigh:166.6f andWeigh:54.3f];
-    Cycler* cycler = [[Cycler alloc] initWithParameters:@"Yaroslav" andSex:@"M" andAge:29 andHeigh:179.22f andWeigh:70.f];
-    Swimer* swimer = [[Swimer alloc] initWithParameters:@"Ustas" andSex:@"M" andAge:45 andHeigh:198.4f andWeigh:122.5f];
-    Biorobot* robot = [[Biorobot alloc] initWithParameters:@"Angela" andSex:@"F" andAge:40 andHeigh:164.4f andWeigh:44.f andTypeOfProthesis:@"Hand"];
-    Animal* defaultAnimal = [[Animal alloc]initWithParameters:12 andLife:100 andSpeed:10];
-    Leopard* leopard = [[Leopard alloc]initWithParameters:4 andLife:100 andSpeed:99];
-    Ox* ox = [[Ox alloc]initWithParameters:19 andLife:100 andSpeed:4];
-
-    
-    NSArray* stackOfObjects = [NSArray arrayWithObjects:defaultHuman, runner, cycler, swimer, robot, defaultAnimal, leopard, ox, nil];
-    for (NSObject* object in stackOfObjects) {
-        NSLog(@"__________________________");
-        NSLog(@"%@", NSStringFromClass([object class]));
-        if ([object isKindOfClass:[Human class]]) {
-            Human* human = (Human* )object;
-            NSLog(@"Name: %@", human.name);
-            NSLog(@"Sex: %@", human.sex);
-            NSLog(@"Age: %ld", human.age);
-            NSLog(@"Heigh: %.01f", human.heigh);
-            NSLog(@"Weigh: %.01f", human.weigh);
-            [human move];
-            if ([human isKindOfClass:[Biorobot class]]) {
-                Biorobot* tempRobot = (Biorobot* )human;
-                NSLog(@"Current battery: %ld%%", tempRobot.powerBalance);
-            }
-        }
-        if ([object isKindOfClass:[Animal class]]) {
-            Animal* animal = (Animal* )object;
-            NSLog(@"Age: %ld", animal.age);
-            NSLog(@"%s", animal.isPredator ? "Predator" : "Herbivorous");
-            NSLog(@"Speed: %ld", animal.speed);
-            NSLog(@"Life: %ld", animal.life);
-            [animal move];
-        }
-    }
     return YES;
 }
 
